e5ac7af2179d62f42f4c79a6348faa672efac90e
Hariprasadchalla <prasadchalla88@gmail.com>
2012-05-22 16:11:02 +0530

    adding to Second repo

---
+++ code/src/HelloWorld1.java
@@ -0,0 +1,10 @@
+public class HelloWorld1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Wohgjgjfrld1122111");
+		 System.out.println("Hello, World1sdes122111");
+		 System.out.println("Hello, Wohgjgjfrld112SDS2111");.
+		 System.out.println("Hello, Wohgjgjfrld1CV12SDS2111");
+    }
+
+}
---
+++ code/src/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, dfdf");
+		 System.out.println("Hello, JKHdfsxsdfdfdfKHKJKWorld1122111");
+    }
+
+}
